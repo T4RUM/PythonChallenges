34b94073d113c8b0eed4cb889035c82e09ce7eeb
T4RUM <wesley-murat@hotmail.com>
2022-02-02 19:33:36 -0300

    Exercicio 003

---
+++ Exercicio_003.py
@@ -0,0 +1,6 @@
+# CRIE UM PROGRAMA QUE LEIA DOIS NÚMEROS E MOSTRE A SOMA ENTRE ELES.
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+resultado = n1 + n2
+print('A soma entre {} e {} é igual a {}!'.format(n1, n2, resultado))
