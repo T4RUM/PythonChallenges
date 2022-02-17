c54500482622c2f8f770452c8be8a38371addde2
T4RUM <wesley-murat@hotmail.com>
2022-02-17 11:02:22 -0300

    Exercicio_007

---
+++ Exercicio_007.py
@@ -0,0 +1,7 @@
+#DESENVOLVA UM PROGRAMA QUE LEIA AS DUAS NOTAS DE UM ALUNO, CALCULE E MOSTRE A SUA MÉDIA
+
+nota1 = float(input('Digite sua primeira nota: '))
+nota2 = float(input('Digite sua segunda nota: '))
+media = (nota1 + nota2) / 2
+
+print('A média de suas notas são: {}'.format(media))
