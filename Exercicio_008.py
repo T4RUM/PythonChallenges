c7d34584b51e59f39e7356f9889345abe7222dbb
T4RUM <wesley-murat@hotmail.com>
2022-02-18 16:45:57 -0300

    Exercicio_008

---
+++ Exercicio_008.py
@@ -0,0 +1,15 @@
+#ESCREVA UM PROGRAMA QUE LEIA UM VALOR EM METROS E O EXIBA CONVERTIDO EM CENTIMETROS E MILIMETROS
+
+
+medida = float(input('Uma distância em metros: '))
+centimetros = medida * 100
+milimetros = medida * 1000
+print(f'A medida de {medida}m corresponde a {centimetros}cm')
+print(f'A medida de {medida}m corresponde a {milimetros}mm')
+
+
+
+#print('O valor em centimetros é = {}'.format(centimetros))
+#print('O valor em milimetros é = {}'.format(milimetros))
+
+
