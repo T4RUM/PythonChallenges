aef1869d42cca2209f49dbe86b0248a71bab6500
Wesley Murat <71726125+T4RUM@users.noreply.github.com>
2022-06-12 18:35:55 -0300

    Exercicio_025

---
+++ Exercicio_025.py
@@ -0,0 +1,4 @@
+# CRIE UM PROGRAMA QUE LEIA O NOME DE UMA PESSOA E DIGA SE ELA TEM "SILVA" NO NOME.
+
+nome = str(input('Qual é seu nome completo?  ')).strip()
+print('Seu nome tem Silva? {}'.format('silva' in nome.lower()))
