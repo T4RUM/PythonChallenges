e1a41c13920d2ca64ad913a0e7b6cb9cb814d42e
Wesley Murat <71726125+T4RUM@users.noreply.github.com>
2022-06-12 18:46:44 -0300

    Exercicio_038

--- Exercicio_038.py
+++ Exercicio_038.py
@@ -1,5 +1,5 @@
 # Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensgem:
-# - O primeiro valor é maior
+# - O primeiro valor é maior 
 # - O segundo valor é maior
 # Não existe valor maior, os dois são iguais
 
