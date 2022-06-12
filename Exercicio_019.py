7f302da14adbbd9ab27371b90c10feb70bab1282
Wesley Murat <71726125+T4RUM@users.noreply.github.com>
2022-06-12 18:34:25 -0300

    Exercicio_019

---
+++ Exercicio_019.py
@@ -0,0 +1,16 @@
+# UM PROFESSOR QUER SORTEAR UM DOS SEUS QUATRO ALUNOS PARA APAGAR O QUADRO
+# FAÇA UM PROGRAMA QUE AJUDE ELE, LENDO O NOME DELES E ESCREVENDO O NOME DO ESCOLHIDO
+
+import random
+aluno1 = input('Nome do primeiro aluno: ')
+aluno2 = input('Nome do segundo aluno: ')
+aluno3 = input('Nome do terceiro aluno: ')
+aluno4 = input('Nome do quarto aluno: ')
+sorteio = random.choices([aluno1, aluno2, aluno3, aluno4])
+print('O aluno sorteado para apagar a lousa foi: {}'. format(sorteio))
+
+
+
+# OUTRO MODO DE SE FAZER ISSO POREM DE MANEIRA QUE FICA FIXO OS NOMES
+'''const = ['Wesley','Elias', 'Alessandra', 'Rosimeire']'''
+'''sorteio = random.choice(const)'''
